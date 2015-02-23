fed9cdc3480682a56e7267b71437cb407d9579be
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-23 07:42:16 -0500

    Added env file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var io = require('socket.io').listen(5001);
-var redis = require('redis').createClient();
+var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 redis.subscribe('rt-change');
 
