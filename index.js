3be02f34508fa24dbefe2725cb3532e5b40505a4
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-30 19:01:37 -0400

    Added PORT env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var io = require('socket.io').listen(process.env.APP_PORT);
+var io = require('socket.io').listen(process.env.PORT || process.env.APP_PORT);
+console.log("Socket.io Listening on " + (process.env.PORT || process.env.APP_PORT));
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
   var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
 } else {
   var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 }
