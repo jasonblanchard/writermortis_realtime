5c87a99f81c7a23b19a7e987a0932dbd206df571
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-30 18:47:28 -0400

    Added redistogo config

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var io = require('socket.io').listen(process.env.APP_PORT);
-var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+} else {
+  var redis = require('redis').createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+}
 
 redis.subscribe('rt-change');
 
